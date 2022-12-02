ea61aa16c655bd33d075b88215f0afd143127034
Шамиль Атауллин <shamil@MacBook-Air.local>
2022-12-02 09:14:02 +0300

    ex01

--- CPP_Module_02/ex01/Fixed.cpp
+++ CPP_Module_02/ex01/Fixed.cpp
@@ -6,7 +6,61 @@
 /*   By: ntojamur <ntojamur@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 08:16:05 by ntojamur          #+#    #+#             */
-/*   Updated: 2022/11/29 08:16:06 by ntojamur         ###   ########.fr       */
+/*   Updated: 2022/12/02 09:11:40 by ntojamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(){
+	fixed_val = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int const val){
+	fixed_val = val << raw;
+	std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(float const val){
+	std::cout << "Float constructor called\n";
+	this->fixed_val = roundf(val * (1 << Fixed::raw));
+}
+
+Fixed::Fixed(const Fixed &old){
+	std::cout << "Copy constructor called\n";
+	*this = old;
+}
+
+Fixed::~Fixed(void){
+	std::cout << "Destructor called" << std::endl;
+}
+
+float Fixed::toFloat( void ) const{
+	return ((float)this->fixed_val/(float)(1 << Fixed::raw));
+}
+
+int Fixed::toInt( void ) const{
+	return (this->fixed_val >> Fixed::raw);
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed const &value){
+	out << value.toFloat();
+	return (out);
+}
+
+int	Fixed::getRawBits(void) const{
+	return (this->fixed_val);
+}
+
+void Fixed::setRawBits( int const raw ){
+	this->fixed_val = raw;
+}
+
+Fixed &Fixed::operator= (const Fixed &old){
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->fixed_val = old.getRawBits();
+	return (*this);
+}
+
 
--- CPP_Module_02/ex01/Fixed.hpp
+++ CPP_Module_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ntojamur <ntojamur@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 08:16:07 by ntojamur          #+#    #+#             */
-/*   Updated: 2022/11/29 08:18:08 by ntojamur         ###   ########.fr       */
+/*   Updated: 2022/12/02 09:13:12 by ntojamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,28 @@
 #define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
-class Fixed{
+class Fixed
+{
 	private:
+		static const int raw = 8;
+		int fixed_val;
 
 	public:
+		Fixed();
+		Fixed(int const val);
+		Fixed(float const val);
+		Fixed(const Fixed &old);
+		~Fixed();
+
+		float toFloat( void ) const;
+		int toInt( void ) const;
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		Fixed &operator= (const Fixed &old);
 };
+
+std::ostream &operator<<(std::ostream &out, Fixed const &value);
 
 #endif
