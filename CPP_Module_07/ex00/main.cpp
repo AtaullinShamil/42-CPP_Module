d8c2b421192d84cb5564dc2a3e8255fb0c5cbbc7
Шамиль Атауллин <shamil@MacBook-Air.local>
2023-01-16 09:24:36 +0300

    ex00

--- CPP_Module_07/ex00/main.cpp
+++ CPP_Module_07/ex00/main.cpp
@@ -6,43 +6,25 @@
 /*   By: ntojamur <ntojamur@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 08:38:02 by ntojamur          #+#    #+#             */
-/*   Updated: 2023/01/16 09:19:31 by ntojamur         ###   ########.fr       */
+/*   Updated: 2023/01/16 09:23:40 by ntojamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-int main()
-{
-	int		i_a = 2;
-	int		i_b = 3;
-	float	f_a = 2.3;
-	float	f_b = 3.2;
+int main( void ) {
+	int a = 2;
+	int b = 3;
 
-	std::cout << "\nswap :" << std::endl;
-
-	std::cout << "---int---" << std::endl;
-	std::cout <<"a = " << i_a << ", b = " << i_b << std::endl;
-	swap(i_a, i_b);
-	std::cout <<"a = " << i_a << ", b = " << i_b << std::endl;
-	std::cout << "--float--" << std::endl;
-	std::cout <<"a = " << f_a << ", b = " << f_b << std::endl;
-	swap(f_a, f_b);
-	std::cout <<"a = " << f_a << ", b = " << f_b << std::endl;
-
-	std::cout << "\nmin :" << std::endl;
-
-	std::cout << "---int---" << std::endl;
-	std::cout << "a = " << i_a << ", b = " << i_b << ", min = "<< min(i_a, i_b) << std::endl;
-	std::cout << "--float--" << std::endl;
-	std::cout << "a = " << f_a << ", b = " << f_b << ", min = "<< min(f_a, f_b) << std::endl;
-
-	std::cout << "\nmax :" << std::endl;
-
-	std::cout << "---int---" << std::endl;
-	std::cout << "a = " << i_a << ", b = " << i_b << ", max = "<< max(i_a, i_b) << std::endl;
-	std::cout << "--float--" << std::endl;
-	std::cout << "a = " << f_a << ", b = " << f_b << ", max = "<< max(f_a, f_b) << std::endl;
-
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return (0);
 }
